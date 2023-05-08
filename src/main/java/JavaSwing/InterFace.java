87de9389b45794fd33c616f2f214ab93b7ac2515
Angel Gabriel <alquisira2004@gmail.com>
2023-05-07 19:41:45 -0600

    Se hace uso el swing java con pantallas

---
+++ src/main/java/JavaSwing/InterFace.java
@@ -0,0 +1,16 @@
+package JavaSwing;
+public class InterFace {
+    public static void main(String[] args) {
+
+        /*HolaMundo p2 = new HolaMundo();
+        v2.setVisible(true);*/
+        Pantalla p1 = new Pantalla();
+        p1.setVisible(true);
+      /*  HolaMundo2 p3= new HolaMundo2();
+        p3.setVisible(true);*/
+
+
+
+    }
+}
+
